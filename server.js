66c56e908247cbf46670c88fb7823eda8f8247fa
Tez28 <courtezcannady@gmail.com>
2022-03-12 12:26:03 -0500

    user input init

--- server.js
+++ server.js
@@ -20,3 +20,51 @@ connection.connect(function(err) {
     initApp();
 });
 
+// Initiates application for input
+function initApp() {
+    inquirer
+        .prompt({
+            type: "list",
+            choices: [
+                "View All Department",
+                "View All Roles",
+                "View All Employees",
+                "Add A Department",
+                "Add A Role",
+                "Add A Employee",
+                "Update A Employee",
+                "Done"
+            ],
+            message: "What would you like to do?",
+            name: "choice"
+        })
+        .then(function(result) {
+            console.log("You chose: " +result.choice);
+            switch (result.choice) {
+                case "View All Department":
+                    viewAllDepartment();
+                    break;
+                case "View All Roles":
+                    viewAllRoles();
+                    break;
+                case "View All Employees":
+                    viewAllEmployees();
+                    break;
+                case "View A Department":
+                    viewDepartment();
+                    break;
+                case "View A Role":
+                    viewRole();
+                    break;
+                case "View A Employee":
+                    viewEmployee();
+                    break;
+                case "Update employee role":
+                    updateEmployee();
+                    break;
+                default:
+                    done();
+
+            }
+        });
+}
