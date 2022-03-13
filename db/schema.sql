2808baae147ad4d10099587b8098e4e2a5084400
Tez28 <courtezcannady@gmail.com>
2022-03-13 14:03:45 -0400

    add role table

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,10 @@ CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
 );
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT NOT NULL,
+);
